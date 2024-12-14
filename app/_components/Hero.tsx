a15a47b5c73ea4789bd2e2c05104e687c78b23db
mirada725 <miradakariyawasam725@gmail.com>
2024-12-14 20:08:42 +0530

    add create team functionality
    Implemented the 'Create Team' feature that allows users to create teams by entering a team name. This includes backend validation and integration with the database. The user is redirected to the dashboard upon successful team creation.

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -10,14 +10,13 @@ const Hero = () => {
       <h1
         className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
       >
-        Understand User Flow.
+        Simplify the design
 
-        <span className="sm:block"> Increase Conversion. </span>
+        <span className="sm:block">  Streamline Panel Creation. </span>
       </h1>
 
       <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
-        numquam ea!
+      Unlock efficiency and precision in your electrical design projects. Effortlessly create, customize, and get quotes for your electrical panel boards!
       </p>
 
       <div className="mt-8 flex flex-wrap justify-center gap-4">
